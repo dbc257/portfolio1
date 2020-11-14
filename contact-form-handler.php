22ca9df8484df1bd60e933aa64a1809904f78dfd
DasomAnH <ektha116@gmail.com>
2020-11-14 12:55:31 -0500

    update

---
+++ contact-form-handler.php
@@ -0,0 +1,20 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'ektha@gmail.com';
+    $email_subject = 'New Form Submission';
+    $email_body = "User Name: $name.\n".
+                      "User Email: $visitor_email.\n".
+                          "User Message: $message.\n";
+
+    $to = 'dasom.huber@gmail.com';
+    $headers = 'From: $email_from\r\n';
+    $headers .= 'Relpy-To: $visitor_email \r\n';
+
+    mail($to,$email_subject,$email_body,$headers);
+    header('Location:index.html');
+
+
+?>
